07ee4f8467449ce11d3d48eec8f72be460c262cb
Bailey Miller <bmiller@susquehannasoftware.com>
2018-04-30 21:45:48 -0400

    Toast on like error

--- src/app/UI/components/blog-preview/blog-preview.component.ts
+++ src/app/UI/components/blog-preview/blog-preview.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Blog } from '../../../_Models/blog.model';
+import { AuthService } from '../../../_Services/auth.service';
+import { SnackBarService } from '../../../_Services/snack-bar.service';
 
 @Component({
   selector: 'blog-preview',
@@ -12,7 +14,7 @@ export class BlogPreviewComponent implements OnInit {
   public Blog: Blog;
   public DisplayLoading: boolean = true;
 
-  constructor()
+  constructor(public Auth: AuthService, private toast: SnackBarService)
   {
     
   }
@@ -20,6 +22,17 @@ export class BlogPreviewComponent implements OnInit {
   LoadEvent(data: any)
   { 
     this.DisplayLoading = false;
+  }
+
+  public ToggleLike(): void
+  {
+    if (this.Auth.IsAuthenticated())
+    { 
+      this.Blog.ToggleLike();
+    } else
+    {
+      this.toast.openSnackBar("You need to be authenticated to like/dislike articles!", "Dismiss", 2);
+    }  
   }
 
   ngOnInit() 
